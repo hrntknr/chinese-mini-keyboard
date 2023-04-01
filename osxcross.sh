57e9c9e80d88857163251b33df2d463c9f0f536f
Takanori Hirano <me@hrntknr.net>
2023-04-01 13:24:31 +0900

    libusb

---
+++ osxcross.sh
@@ -0,0 +1,55 @@
+#!/bin/bash -eux
+cd $(dirname $0)
+
+case "$PLATFORM" in
+x86_64*)
+  ARCH="x86_64"
+  GOARCH="amd64"
+  ;;
+arm64*)
+  ARCH="arm64"
+  GOARCH="arm64"
+  ;;
+esac
+
+git clone https://github.com/tpoechtrager/osxcross.git
+(
+  cd osxcross
+  OSXCROSS=$(pwd)
+
+  sudo ./tools/get_dependencies.sh
+  wget -q https://github.com/phracker/MacOSX-SDKs/releases/download/$SDK_VERSION/MacOSX$SDK_VERSION.sdk.tar.xz -O tarballs/MacOSX$SDK_VERSION.sdk.tar.xz
+
+  UNATTENDED=1 ./build.sh
+
+  export PATH="$OSXCROSS/target/bin:$PATH"
+  export MACOSX_DEPLOYMENT_TARGET="$SDK_VERSION"
+
+  sed -i -e "s/ARCH=\"x86_64\"/ARCH=\"$ARCH\"/" $OSXCROSS/target/bin/osxcross-macports
+  sed -i -e 's/verifyFileIntegrity "$pkgfile"/#verifyFileIntegrity "$pkgfile"/' $OSXCROSS/target/bin/osxcross-macports
+
+  UNATTENDED=1 osxcross-macports install libusb
+
+  export OSXCROSS_MP_INC="1"
+  export CC="o64-clang"
+  export CXX="o64-clang++"
+  export AR="$PLATFORM-ar"
+  export LD="$PLATFORM-ld"
+  export CGO_ENABLED="1"
+  export GOOS="darwin"
+  export GOARCH="$GOARCH"
+
+  if [ -e env.sh ]; then
+    rm env.sh
+  fi
+  echo "echo 'PATH=$PATH' >>\$GITHUB_ENV" >>env.sh
+  echo "echo 'OSXCROSS_MP_INC=$OSXCROSS_MP_INC' >>\$GITHUB_ENV" >>env.sh
+  echo "echo 'CC=$CC' >>\$GITHUB_ENV" >>env.sh
+  echo "echo 'CXX=$CXX' >>\$GITHUB_ENV" >>env.sh
+  echo "echo 'AR=$AR' >>\$GITHUB_ENV" >>env.sh
+  echo "echo 'LD=$LD' >>\$GITHUB_ENV" >>env.sh
+  echo "echo 'CGO_ENABLED=$CGO_ENABLED' >>\$GITHUB_ENV" >>env.sh
+  echo "echo 'GOOS=$GOOS' >>\$GITHUB_ENV" >>env.sh
+  echo "echo 'GOARCH=$GOARCH' >>\$GITHUB_ENV" >>env.sh
+  chmod +x env.sh
+)
